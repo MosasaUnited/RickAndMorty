d96db45447ef637166287b963d24588f5571dffd
Mostafa Mahmoud Saad <79766907+MosasaUnited@users.noreply.github.com>
2024-05-18 20:21:34 +0300

    Start App

---
+++ lib/rick_and_morty.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'home/home_screen.dart';
+
+class RickAndMorty extends StatelessWidget {
+  const RickAndMorty({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Rick And Morty',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
