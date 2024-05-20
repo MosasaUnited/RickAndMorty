55b166d27e6d581b87bd9f31948ba325ad5975b1
Mostafa Mahmoud Saad <79766907+MosasaUnited@users.noreply.github.com>
2024-05-20 21:57:46 +0300

    BlocProviderInitializeInAppRouter

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,13 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:rick_and_morty/features/home/data/repo/characters_repo.dart';
+import 'package:rick_and_morty/features/home/logic/cubit/characters_cubit.dart';
 
+import '../../features/home/data/services/characters_web_services.dart';
 import '../../features/home/home_screen.dart';
 import '../../features/home/presentation/screens/characters.dart';
 import '../../features/onboarding/onboarding_screen.dart';
+
+late CharactersRepo charactersRepo;
+late CharactersCubit charactersCubit;
 
 abstract class AppRouter {
   static const kOnboarding = '/';
   static const kHome = '/home';
   static const kCharacters = '/characters';
+
+  static void initAppRouter() {
+    charactersRepo = CharactersRepo(CharactersWebServices());
+    charactersCubit = CharactersCubit(charactersRepo);
+  }
 
   static final router = GoRouter(
     routes: [
@@ -21,7 +34,9 @@ abstract class AppRouter {
       ),
       GoRoute(
         path: kCharacters,
-        builder: (context, state) => const CharactersScreen(),
+        builder: (context, state) => BlocProvider(
+            create: (BuildContext context) => charactersCubit,
+            child: const CharactersScreen()),
       ),
     ],
   );
