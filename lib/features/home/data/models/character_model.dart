dee68a892ea03cbe4b3a3c2f42fb9d7e8b11ef60
Mostafa Mahmoud Saad <79766907+MosasaUnited@users.noreply.github.com>
2024-05-20 10:49:52 +0300

    AddCharacterModel

--- lib/features/home/data/models/character_model.dart
+++ lib/features/home/data/models/character_model.dart
@@ -1,3 +1,25 @@
-class CharacterModel{
+class CharacterModel {
+  late int charId;
+  late String name;
+  late String status;
+  late String species;
+  late String gender;
+  late String location;
+  late String image;
+  late List<String> episode;
+  late String url;
+  late String created;
 
+  CharacterModel.fromJson(Map<String, dynamic> json) {
+    charId = json["id"];
+    name = json["name"];
+    status = json["status"];
+    species = json["species"];
+    gender = json["gender"];
+    location = json["location"];
+    image = json["image"];
+    episode = json["episode"];
+    url = json["url"];
+    created = json["created"];
+  }
 }
